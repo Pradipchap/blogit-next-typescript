bf554f6d007c10edeec3bfa88334dbc671abb2f3
Pradip Chapagain <pradipcpgn@gmail.com>
2024-01-26 16:13:24 +0545

    refactor

--- utils/constants.ts
+++ utils/constants.ts
@@ -9,7 +9,4 @@ export enum SUBMIT_STATUS {
   PROCESSING = "processing",
 }
 
-const LOCAL_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_BASE_URL;
-const PROD_BASE_URL = process.env.NEXT_PUBLIC_PROD_BASE_URL;
-
-export const BASE_URL = PROD_BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
