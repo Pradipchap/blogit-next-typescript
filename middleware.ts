b545fd9bf184524b30ee4b5d8abdc4b6ff2d2aa3
Pradip Chapagain <pradipcpgn@gmail.com>
2024-05-14 02:10:29 +0545

    refactor middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+export { default } from "next-auth/middleware";
 import { NextResponse, type NextRequest } from "next/server";
 import { BASE_URL } from "./utils/constants";
 
@@ -8,62 +9,51 @@ const allowedOrigins = [
   "https://lucent-longma-3d7e84.netlify.app",
 ];
 
-const corsHeaders = {
+const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export async function middleware(request: NextRequest) {
-  try {
-    // Checking authenticated routes
-    const protectedRoutes = ["/create", "/profile"];
-    const userToken = request.cookies.get("next-auth.session-token")?.value;
-    const currentUrl = request.nextUrl.pathname;
-    const isProtectedRoute = protectedRoutes.includes(currentUrl);
+  //checking authenticated routes
 
-    if (isProtectedRoute && !userToken) {
-      return NextResponse.redirect(
-        new URL(`${BASE_URL}/api/auth/signin`, request.nextUrl)
-      );
-    }
+  const redirectUrl = ["/create", "/profile"];
+  const userToken = request.cookies.get("next-auth.session-token")?.value;
+  const currentUrl = request.nextUrl.pathname;
+  const isProtectedUrl = redirectUrl.find((element) => element === currentUrl);
+  const matchUrl = !!isProtectedUrl;
 
-    // Handle CORS
-    const origin = request.headers.get("origin") || "";
-    const isAllowedOrigin = allowedOrigins.includes(origin);
-
-    if (isAllowedOrigin) {
-      request.headers.set("Access-Control-Allow-Origin", origin);
-    }
-
-    // Handle preflight requests
-    if (request.method === "OPTIONS") {
-      const preflightHeaders = {
-        ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-        ...corsHeaders,
-      };
-      return NextResponse.json({}, { headers: preflightHeaders });
-    }
-
-    // Handle simple requests
-    const response = NextResponse.next();
-
-    if (isAllowedOrigin) {
-      response.headers.set("Access-Control-Allow-Origin", origin);
-    }
-
-    Object.entries(corsHeaders).forEach(([key, value]) => {
-      response.headers.set(key, value);
-    });
-
-    return response;
-  } catch (error) {
-    console.error("Error in middleware:", error);
-    return NextResponse.error();
+  if (matchUrl && !userToken) {
+    return NextResponse.redirect(
+      new URL(`${BASE_URL}/api/auth/signin`, request.nextUrl)
+    );
   }
+
+  // const origin = request.headers.get("origin") ?? "";
+  // const isAllowedOrigin = allowedOrigins.includes(origin);
+  // // / Handle preflighted requests
+  // const isPreflight = request.method === "OPTIONS";
+
+  // if (isPreflight) {
+  //   const preflightHeaders = {
+  //     ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+  //     ...corsOptions,
+  //   };
+  //   return NextResponse.json({}, { headers: preflightHeaders });
+  // }
+
+  // // Handle simple requests
+  const response = NextResponse.next();
+
+  // if (isAllowedOrigin) {
+  //   console.log("is allowed origin", origin);
+  //   response.headers.set("Access-Control-Allow-Origin", origin);
+  // }
+
+  // Object.entries(corsOptions).forEach(([key, value]) => {
+  //   response.headers.set(key, value);
+  // });
+  return response;
 }
 
-export const config = { 
-  api: {
-    bodyParser: false, // Disables body parsing, as it's not needed for this middleware
-  },
-};
+export const config = { matcher: ["/profile", "/create", "/api/:path"] };
