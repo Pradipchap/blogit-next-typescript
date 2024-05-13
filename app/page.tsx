d6ca9f8e5fa3131e21f7d49833d9ca6d25e339c2
Pradip Chapagain <pradipcpgn@gmail.com>
2024-05-14 02:53:56 +0545

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,13 @@ export default function Home() {
   return (
     <>
       <div className="h-max flex justify-around w-full mt-5 min-h-[calc(100vh-76px)] px-2 md:px-20">
-        <div className="w-[70%] h-full">
+        {/* <div className="w-[70%] h-full">
           <MainBlogs />
         </div>
         <div className="border-l border-gray-200 sideblog w-[30%] sticky top-[76px] h-[60rem] hidden md:block px-5">
           <Blogpage />
-        </div>
+        </div> */}
+        jjjkjkkj
       </div>
     </>
   );
