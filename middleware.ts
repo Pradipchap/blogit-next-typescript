ac1b47eee675e01ea7ffc81325d42965e2c23930
Pradip Chapagain <110084844+Pradipchap@users.noreply.github.com>
2025-04-24 23:39:35 +0545

    Fix:typo

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ const allowedOrigins = [
   "https://blogit-next-typescript-pradipchaps-projects.vercel.app",
   "https://blogit-next-typescript-pradipchap.vercel.app",
   "https://blogit-next-typescript.vercel.app",
-  "https://*pradipchapagain.com,np"
+  "https://*pradipchapagain.com.np"
 ];
 
 const corsOptions = {
